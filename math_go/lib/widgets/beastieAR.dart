c8fb7207cfda968d5c7dc72c0c06bdf10553413e
Kyle <yoneshky@oregonstate.edu>
2020-05-20 17:09:30 -0700

    ArCore Implementation

--- math_go/lib/widgets/beastieAR.dart
+++ math_go/lib/widgets/beastieAR.dart
@@ -1,47 +1,92 @@
-import 'package:flutter/material.dart';
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
-
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 class BeastieAr extends StatefulWidget {
-  final String title;
-
-  const BeastieAr({Key key, this.title}) : super(key: key);
-
-  State createState() => _BeastieArState();
+  @override
+  _BeastieArState createState() => _BeastieArState();
 }
 
 class _BeastieArState extends State<BeastieAr> {
-  //AR Variables
   ArCoreController arCoreController;
 
   @override
   Widget build(BuildContext context) {
-    Size screenSize = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-          title: const Text('test'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello World'),
         ),
         body: ArCoreView(
           onArCoreViewCreated: _onArCoreViewCreated,
-          enableTapRecognizer: true,
         ),
-      );
+      ),
+    );
   }
 
   void _onArCoreViewCreated(ArCoreController controller) {
     arCoreController = controller;
-    arCoreController.onNodeTap = (name) => onTapHandler(name);
+
+    _addSphere(arCoreController);
+    _addCylindre(arCoreController);
+    _addCube(arCoreController);
   }
 
-  void onTapHandler(String name) {
-    showDialog<void>(
-      context: context,
-      builder: (BuildContext context) =>
-          AlertDialog(content: Text('onNodeTap on $name')),
+  void _addSphere(ArCoreController controller) {
+    final material = ArCoreMaterial(
+        color: Color.fromARGB(120, 66, 134, 244), 
+        //texture: "plane-piloe.png"
     );
+    final sphere = ArCoreSphere(
+      materials: [material],
+      radius: 0.1,
+    );
+    final node = ArCoreNode(
+      shape: sphere,
+      position: vector.Vector3(0, 0, -1.5),
+      // children: [
+      //   new Image.asset('assets/plane-pilot.png'),
+      // ]
+    );
+    controller.addArCoreNode(node);
+  }
+
+  void _addCylindre(ArCoreController controller) {
+    final material = ArCoreMaterial(
+      color: Colors.red,
+      reflectance: 1.0,
+    );
+    final cylindre = ArCoreCylinder(
+      materials: [material],
+      radius: 0.5,
+      height: 0.3,
+    );
+    final node = ArCoreNode(
+      shape: cylindre,
+      position: vector.Vector3(0.0, -0.5, -2.0),
+    );
+    controller.addArCoreNode(node);
+  }
+
+  void _addCube(ArCoreController controller) {
+    final material = ArCoreMaterial(
+      color: Color.fromARGB(120, 66, 134, 244),
+      metallic: 1.0,
+    );
+    final cube = ArCoreCube(
+      materials: [material],
+      size: vector.Vector3(0.5, 0.5, 0.5),
+    );
+    final node = ArCoreNode(
+      shape: cube,
+      position: vector.Vector3(-0.5, 0.5, -3.5),
+    );
+    controller.addArCoreNode(node);
+  }
+
+  @override
+  void dispose() {
+    arCoreController.dispose();
+    super.dispose();
   }
 }
-
-//FIX ME:
-//Below is potential fix to sigterm exit
-//https://stackoverflow.com/questions/61167711/sample-flutter-ar-project-app-crashes-unable-to-start-activity-componentinfo
