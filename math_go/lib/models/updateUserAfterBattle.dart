90ebc6adcce5d23936a9e612198469ad771c97fd
jpybe5 <jacksonpyburn@gmail.com>
2020-05-27 08:50:32 -0700

    Add updateUserAfterBattle function

---
+++ math_go/lib/models/updateUserAfterBattle.dart
@@ -0,0 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/getBeasties.dart';
+
+Future<bool> updateUserAfterBattle (String username, bool captured, beastieInfo currentBeastie) async{
+  
+  bool updateSuccessful = false;
+  final Firestore _mathGoStore = Firestore.instance;
+
+  DocumentReference docRef = _mathGoStore.collection("users").document(username);
+    await docRef.get().then((DocumentSnapshot datasnapshot) {
+              if (datasnapshot.exists) {
+                if(captured==false){
+                    _mathGoStore.collection("users")
+                      .document(username)
+                      .updateData({
+                        'questionAttempt': (datasnapshot.data['questionAttempt']+1)
+                      });
+                }
+                else if(captured==true){
+                    _mathGoStore.collection("users")
+                      .document(username).updateData({
+                        'questionAttempt': (datasnapshot.data['questionAttempt']+1),
+                        'questionCorrect': (datasnapshot.data['questionCorrect']+1)
+                      });
+
+                   _mathGoStore.collection("users")
+                      .document(username).collection("myBeasties").add({
+                        'name': currentBeastie.name,
+                        ////////////add image path when its added to beastieInfo object    
+                      });
+                }
+                updateSuccessful=true;
+              }
+              else{
+                updateSuccessful=false;
+              }
+    });
+    
+
+  return updateSuccessful;
+}
+
